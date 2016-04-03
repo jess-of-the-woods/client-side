c9fcc44a0026c89c867c71c84ac801e5de3b78c3
Jess Stuart <email": null>
2016-04-04 11:31:44 +1200

    Updates

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var example = require('./views/example.hbs')
 xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
   if (err) console.log(err) // do something
   var usefulData = JSON.parse(data.body)
-  console.log(data.body)
-  console.log(usefulData.name)
-  //console.log(usefulData[0].name)
-  document.body.innerHTML = example({ name: usefulData.name });
-  document.body.innerHTML = example({ latitude: usefulData.latitude });
+  console.log('this is data.body: ', data.body)
+  console.log('this is usefulData.name: ', usefulData.name)
+  console.log('this is usefulData.latitude: ', usefulData.latitude)
+  document.body.innerHTML = example({ name: usefulData.name, latitude: usefulData.latitude });
 })
 
