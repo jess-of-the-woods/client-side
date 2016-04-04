8bcbbde929517569b788e27bbf6ddbffc264ac20
Jess Stuart <email": null>
2016-04-04 16:50:31 +1200

    get release 3 working, add button to page with listener

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-  if (err) console.log(err) // do something
 
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+var button = document.getElementById("displayButton");
+button.addEventListener("click", function(){
+    xhr.get('http://localhost:3000/cats/', function(err, data) {
+    if (err) console.log(err) // do something
+    var usefulData = JSON.parse(data.body)
+    console.log('this is usefulData.cats[0].name: ', usefulData.cats[0].name);
+    console.log('this is usefulData: ', usefulData);
+    document.body.innerHTML = example({ name: usefulData.cats[0].name});
+  })
 })
 
